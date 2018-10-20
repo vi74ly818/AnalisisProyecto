5a09677343a59b0c4661236e9949080a06edeb4d
vi74ly818 <vitaly818@hotmail.com>
2018-10-20 12:30:54 -0500

    Adjunte el paquete estructuras

--- src/vista/pnlMetodosOrdenacion.java
+++ src/vista/pnlMetodosOrdenacion.java
@@ -15,7 +15,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultListModel;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 import modelo.Cancion;
+import modelo.Tiempos_Medidos;
 
 /**
  *
@@ -382,7 +384,7 @@ public class pnlMetodosOrdenacion extends javax.swing.JPanel {
                     lblCantidadElementos.setText((String) jCBCantidadElementos.getSelectedItem());
                 }
             }
-             if (jCBTipoOrdenamiento.getSelectedIndex() == 1 || jCBTipoOrdenamiento.getSelectedIndex() == 2 || jCBTipoOrdenamiento.getSelectedIndex() == 3 || jCBTipoOrdenamiento.getSelectedIndex() == 4
+            if (jCBTipoOrdenamiento.getSelectedIndex() == 1 || jCBTipoOrdenamiento.getSelectedIndex() == 2 || jCBTipoOrdenamiento.getSelectedIndex() == 3 || jCBTipoOrdenamiento.getSelectedIndex() == 4
                     || jCBTipoOrdenamiento.getSelectedIndex() == 5 || jCBTipoOrdenamiento.getSelectedIndex() == 6) {
                 if (jCBTipoDatoOrdenar.getSelectedIndex() == 3) {
                     OrdenarFecha(numerico, jCBTipoOrdenamiento.getSelectedItem().toString());
@@ -841,7 +843,7 @@ public class pnlMetodosOrdenacion extends javax.swing.JPanel {
         }
     }
 
-    public void llenarLista() throws ClassNotFoundException, SQLException {
+      public void llenarLista() throws ClassNotFoundException, SQLException {
         numerico = Integer.parseInt((String) jCBCantidadElementos.getSelectedItem());
 
         cancionArrayList = consulta.selectCancion(numerico);
@@ -852,5 +854,4 @@ public class pnlMetodosOrdenacion extends javax.swing.JPanel {
         jLDatosOriginales.setModel(tabla);
 
     }
-
 }
